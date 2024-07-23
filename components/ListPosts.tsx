9ef556cdf21222b1099bd6e1eec0d9e63af03e29
vKarich2 <vladyslav.karapetov@nure.ua>
2024-07-23 15:12:08 +0200

    refactor(components): simplify and clean up PostsList component

--- components/ListPosts.tsx
+++ components/ListPosts.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Post } from "@/types/Post";
 import SmallPost from "@/components/posts/SmallPost";
 import MediumPost from "@/components/posts/MediumPost";
@@ -10,13 +12,21 @@ interface PostsListProps {
 
 const PostsList: React.FC<PostsListProps> = ({ posts }: PostsListProps) => {
 	const blogLayout = [LargePost, SmallPost, MediumPost, AlternativePost, AlternativePost, MediumPost, SmallPost];
+	console.log(`Number of posts: ${posts.length}`);
+
+	const mlStyle = (index: number) => {
+		if (index >= 7 && ((index - 7) % 3 === 1 || (index - 7) % 3 === 2)) {
+			return { marginLeft: '15px' };
+		}
+		return {};
+	};
 
 	return (
-		<div className={`flex flex-wrap justify-center gap-[60px]`}>
+		<div className={`flex flex-wrap justify-start gap-[60px]`}>
 			{posts.map((post, index) => {
 				const Layout = blogLayout[index] || SmallPost;
 				return (
-					<div key={post.id}>
+					<div key={post.id} style={mlStyle(index)}>
 						<Layout post={post} />
 					</div>
 				);
